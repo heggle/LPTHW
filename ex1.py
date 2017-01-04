e707bd434f4cadfc98f59f6501bd7c2dfaa5eca5
heggle <hilaryeg@gmail.com>
2017-01-03 16:20:46 -0800

    add ex1.py

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Helllo Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Comments typed like this. Doesnt' matter what kind of quotes you use in python, as long as it's a different
+#		type within the original type. 
