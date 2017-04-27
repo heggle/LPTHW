1cc90c1e66a345573bec82d1a46f5cd068cdabb4
heggle <hilaryeg@gmail.com>
2017-04-26 23:23:54 -0700

    Reviewed Ex.4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LPTHW
+The following are exercises done from "Learning Python the Hard Way" to brush up on python syntax.
--- ex4.py
+++ ex4.py
@@ -1,26 +1,11 @@
-#This exercise is for variables and names
+# This is exercise #4 in Learning Python the Hard Way. This exercise looks at Variables and Names. 
 
-#create an integer variable 
-cars = 100
+# 
+cars = 100 
+
+space_in_a_car = 4.0
 drivers = 30
 passengers = 90
-
-#create a float variable called "space in car"
-	#(what would this even be? Occupants as a float??)
-space_in_a_car = 4.0
-
-#create variables using other variables or math
-cars_not_driven = cars - drivers #integer
-cars_driven = drivers #integer
-carpool_capacity = cars_driven*space_in_a_car #float
-average_passengers_per_car = passengers/cars_driven #integer
-
-print "There are", cars, "cars available."
-print "There are only", drivers, "drivers available."
-print "There will be", cars_not_driven, "empty cars today."
-print "We can transport", carpool_capacity, "people today."
-print "We have", passengers, "to carpool today."
-print "We to need to put about", average_passengers_per_car, "in each car."
-
-
-
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool capacity = cars_driven * space_in_a_car
